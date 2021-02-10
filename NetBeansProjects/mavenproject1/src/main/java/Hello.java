0311ecea66fb5424a0ff5e336024749a98d06728
CMD <77295843+christianmarkdc@users.noreply.github.com>
2021-02-09 18:39:59 -0800

    Hello, World! in Java

---
+++ NetBeansProjects/mavenproject1/src/main/java/Hello.java
@@ -0,0 +1,16 @@
+/*
+ *Christian Dela Cruz
+ *Email: christianmarkdc@gmail.com
+ *Date: Feburary 9. 2021
+ *Purpose: My First Hello World program in an IDE
+ */
+
+/**
+ *
+ * @author Christian
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Christian!");
+    }
+}
